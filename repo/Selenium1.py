a6cfce5cab42b56519d9f4006652248e83f3aa2f
Karol <kmdanielak@gmail.com>
2023-02-11 14:27:52 +0100

    Final

--- repo/Selenium1.py
+++ repo/Selenium1.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
@@ -7,11 +7,10 @@ driver.get("https://google.com")
 print("Nazwa strony:", driver.title)
 button1_accept = driver.find_element("id", "L2AGLb")
 button1_accept.click()
-print(button1_accept)
-pole_szukaj = driver.find_element(By.NAME("q"))
-print(pole_szukaj)
-time.sleep(500)
+# print(button1_accept)
+pole_szukaj = driver.find_element("name", "q")
+pole_szukaj.send_keys("python")
+przycisk_szukaj = driver.find_element("name", "btnK")
+przycisk_szukaj.submit()
+time.sleep(1)
 driver.quit()
-
-
-# <button id="L2AGLb" class="tHlp8d" data-ved="0ahUKEwi2r_eSwI39AhVIh_0HHXC8DOIQiZAHCB8"><div class="QS5gu sy4vM" role="none">Zaakceptuj wszystko</div></button>
