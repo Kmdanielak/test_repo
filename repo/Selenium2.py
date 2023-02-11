e3efa5d3b694e6a3ab1c980ba5aac0e57e09c4c9
Karol <kmdanielak@gmail.com>
2023-02-11 15:09:56 +0100

    Final na koniec dnia 11_02_2023

--- repo/Selenium2.py
+++ repo/Selenium2.py
@@ -1,26 +1,28 @@
 from selenium import webdriver
 # from selenium.webdriver.common.by import By
 import time
+import datetime
+
+teraz = datetime.datetime.now()
+
 
 driver = webdriver.Chrome()
 driver.get("http://www.saucedemo.com")
 print("Nazwa strony:", driver.title)
-pole_user = driver.find_element("id", "user-name")
-pole_user.clear()
-pole_user.send_keys("standard_user")
+screenshot = driver.title + teraz.strftime(" %d-%b-%y-%H%M%S") + ".png"
+try:
+    pole_user = driver.find_element("id", "user-name")
+    pole_user.clear()
+    pole_user.send_keys("standard_user")
+except:
+    driver.get_screenshot_as_file(screenshot)
 pole_haslo = driver.find_element("id", "password")
 pole_haslo.clear()
 pole_haslo.send_keys("secret_sauce")
 pole_login = driver.find_element("id", "login-button")
 pole_login.click()
-
-time.sleep(10)
+time.sleep(1)
+driver.get_screenshot_as_file(screenshot)
 driver.quit()
 
-# button1_accept = driver.find_element("id", "L2AGLb")
-# button1_accept.click()
-# # print(button1_accept)
-# pole_szukaj = driver.find_element("name", "q")
-# pole_szukaj.send_keys("python")
-# przycisk_szukaj = driver.find_element("name", "btnK")
-# przycisk_szukaj.submit()
+
