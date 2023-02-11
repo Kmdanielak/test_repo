e53629e1e28973ebef189a023eb7eb65fec01c4b
Karol <kmdanielak@gmail.com>
2023-02-11 11:51:14 +0100

    Commit

--- repo/TDD_tests.py
+++ repo/TDD_tests.py
@@ -1,21 +1,31 @@
-# import functions
+# # import functions
 from functions import *
+#
+#
+# # print(functions.multiply(5, 6))
+# # print(multiply(6, 6))
+#
+#
+# def test_multiply():
+#     assert multiply(5, 10) == 50
+#     assert multiply(100, 1.1) == 110
+#     assert multiply(1.5, 1.5) == 2.25
+#     assert multiply(0.000001, 100) == 0.0001
+#     assert multiply("mama", 3) == "mamamamamama"
+#     assert multiply(None, 5) == None
+#     # nie odpowiedzieliśmy na ten warunek
+#
+# def test_number_of_letters():
+#     assert no_of_letter("mama") == 4
+#     assert no_of_letter("mama.tata") == 8
+#     # nie odpowiedzieliśmy na ten warunek
 
-
-# print(functions.multiply(5, 6))
-# print(multiply(6, 6))
-
-
-def test_multiply():
-    assert multiply(5, 10) == 50
-    assert multiply(100, 1.1) == 110
-    assert multiply(1.5, 1.5) == 2.25
-    assert multiply(0.000001, 100) == 0.0001
-    assert multiply("mama", 3) == "mamamamamama"
-    assert multiply(None, 5) == None
-    # nie odpowiedzieliśmy na ten warunek
-
-def test_number_of_letters():
-    assert no_of_letter("mama") == 4
-    assert no_of_letter("mama.tata") == 8
-    # nie odpowiedzieliśmy na ten warunek
+def test_fizzbuzz():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == "fizz"
+    assert fizzbuzz(4) == 4
+    assert fizzbuzz(5) == "buzz"
+    assert fizzbuzz(6) == "fizz"
+    assert fizzbuzz(10) == "buzz"
+    assert fizzbuzz(15) == "fizzbuzz"
--- repo/functions.py
+++ repo/functions.py
@@ -1,9 +1,19 @@
+#
+# def multiply(x, y):
+#     if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+#         return round(x * y, 5)
+#     else:
+#         return x * y
+#
+# def no_of_letter(text):
+#     return len(text)
 
-def multiply(x, y):
-    if isinstance(x, (int, float)) and isinstance(y, (int, float)):
-        return round(x * y, 5)
+def fizzbuzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+        return "fizzbuzz"
+    elif i % 5 == 0:
+        return "buzz"
+    elif i % 3 == 0:
+        return "fizz"
     else:
-        return x * y
-
-def no_of_letter(text):
-    return len(text)
+        return i
