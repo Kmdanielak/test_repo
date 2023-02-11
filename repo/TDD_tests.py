a84abd192e4cfbf91cba5791898dfad1fba2dfbb
Karol <kmdanielak@gmail.com>
2023-02-11 11:34:09 +0100

    Commit

--- repo/TDD_tests.py
+++ repo/TDD_tests.py
@@ -11,3 +11,11 @@ def test_multiply():
     assert multiply(100, 1.1) == 110
     assert multiply(1.5, 1.5) == 2.25
     assert multiply(0.000001, 100) == 0.0001
+    assert multiply("mama", 3) == "mamamamamama"
+    assert multiply(None, 5) == None
+    # nie odpowiedzieliśmy na ten warunek
+
+def test_number_of_letters():
+    assert no_of_letter("mama") == 4
+    assert no_of_letter("mama.tata") == 8
+    # nie odpowiedzieliśmy na ten warunek
--- repo/functions.py
+++ repo/functions.py
@@ -1,6 +1,9 @@
-def multiply(x, y):
-    return round(x * y, 5)
 
+def multiply(x, y):
+    if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+        return round(x * y, 5)
+    else:
+        return x * y
 
 def no_of_letter(text):
     return len(text)
