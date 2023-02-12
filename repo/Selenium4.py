d43dcde26d68db0a35fd1747d03588e41f63bb6a
Karol <kmdanielak@gmail.com>
2023-02-12 13:32:05 +0100

    final przed robotframework

--- repo/Selenium4.py
+++ repo/Selenium4.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-
 class LoginPage:
     def __init__(self, driver, user="user-name", passwd="secret_sauce", login_button="login-button"):
         self.driver = driver
@@ -7,23 +5,19 @@ class LoginPage:
         self.password_field = passwd
         self.login_button = login_button
 
-
     def open(self):
         self.driver.get("http://www.saucedemo.com")
-
 
     def enter_username(self, username_tresc):
         field = self.driver.find_element("id", self.username_field)
         field.clear()
         field.send_keys(username_tresc)
 
-
     def password(self, password_tresc):
         field = self.driver.find_element("id", self.password_field)
         field.clear()
         field.send_keys(password_tresc)
 
-
     def click_login_button(self):
-        field = self.driver.find_element("id", self.login_button )
+        field = self.driver.find_element("id", self.login_button)
         field.click()
--- repo/Selenium5.py
+++ repo/Selenium5.py
@@ -4,19 +4,23 @@ from Selenium_funkcje import make_screenshot
 from Selenium4 import LoginPage
 import pytest
 
+test_data = [("standard_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"),
+             ("locked_out_user", "secret_sauce", "https://www.saucedemo.com/"),
+             ("problem_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"),
+             ("performance_glitch_user", "secret_sauce", "https://www.saucedemo.com/inventory.html")]
 
-@pytest.mark.parametrize("uzytkownik", ["standard_user", "standard_user2"])
 
-def test_login_page(uzytkownik):
+@pytest.mark.parametrize("user, password, url", test_data)
+def test_login_page(user, password, url):
     driver = webdriver.Chrome()
     nazwa = driver.title
     page = LoginPage(driver, "user-name", "password", "login-button")
     page.open()
-    page.enter_username(uzytkownik)
-    page.password("secret_sauce")
+    page.enter_username(user)
+    page.password(password)
     page.click_login_button()
     time.sleep(1)
     try:
-        assert driver.current_url == "https://www.saucedemo.com/inventory.html", make_screenshot(driver, nazwa)
+        assert driver.current_url == url, make_screenshot(driver, nazwa)
     finally:
         driver.quit()
