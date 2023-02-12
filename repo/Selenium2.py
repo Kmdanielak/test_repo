a7a7a39d4853b64eee8d473be7b1e9596a1beed4
Karol <kmdanielak@gmail.com>
2023-02-12 09:56:33 +0100

    OOP 12_02

--- repo/Selenium2.py
+++ repo/Selenium2.py
@@ -1,28 +1,32 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
 import time
 import datetime
 
-teraz = datetime.datetime.now()
+
+def make_screenshot(driver):
+    teraz = datetime.datetime.now()
+    screenshot = driver.title + teraz.strftime(" %d-%b-%y-%H%M%S") + ".png"
+    driver.get_screenshot_as_file(screenshot)
 
 
 driver = webdriver.Chrome()
 driver.get("http://www.saucedemo.com")
 print("Nazwa strony:", driver.title)
-screenshot = driver.title + teraz.strftime(" %d-%b-%y-%H%M%S") + ".png"
+
 try:
     pole_user = driver.find_element("id", "user-name")
     pole_user.clear()
     pole_user.send_keys("standard_user")
 except:
-    driver.get_screenshot_as_file(screenshot)
-pole_haslo = driver.find_element("id", "password")
+    make_screenshot(driver)
+
+pole_haslo = driver.find_element('xpath','//*[@id="password"]')
 pole_haslo.clear()
 pole_haslo.send_keys("secret_sauce")
 pole_login = driver.find_element("id", "login-button")
 pole_login.click()
 time.sleep(1)
-driver.get_screenshot_as_file(screenshot)
+make_screenshot(driver)
 driver.quit()
 
 
